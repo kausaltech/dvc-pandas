4158ba307e10f871c7472dc9662084c05c4ba00e
Bernhard Bliem <bernhard@bliem.xyz>
2021-04-06 14:06:44 +0300

    Create cache directory if it does not exist

--- src/dvc_pandas/git.py
+++ src/dvc_pandas/git.py
@@ -1,4 +1,6 @@
 import git
+import hashlib
+import os
 from appdirs import user_cache_dir
 
 
@@ -8,6 +10,7 @@ CACHE_DIR = user_cache_dir('dvc-pandas', 'kausaltech')
 def repo_path(url):
     scheme, path = url.split(':', 1)
     path = path.strip('/')
+    path = hashlib.md5(path.encode('utf-8')).hexdigest()
     return f'{scheme}/{path}'
 
 
@@ -17,6 +20,10 @@ def repo_cache_dir(url):
 
 def get_repo(url):
     """Get repo from cache, clone it if it is not cached yet"""
+    try:
+        os.mkdir(CACHE_DIR)
+    except FileExistsError:
+        pass
     repo_dir = repo_cache_dir(url)
     try:
         repo = git.Repo(repo_dir)
