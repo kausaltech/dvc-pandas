2a801a55f405946dc224fb2f7e4bc04993aceb17
Bernhard Bliem <bernhard@bliem.xyz>
2021-10-07 16:25:19 +0200

    Avoid concurrency issues with lock files

--- src/dvc_pandas/repository.py
+++ src/dvc_pandas/repository.py
@@ -1,13 +1,12 @@
+import dvc.repo
+import fasteners
 import logging
 import os
+import pandas as pd
 from pathlib import Path
 from datetime import datetime, timezone
 from typing import List, Optional
-
-import pandas as pd
 from ruamel.yaml import YAML
-
-import dvc.repo
 
 from .dataset import Dataset
 from .dvc import set_dvc_file_metadata
@@ -32,6 +31,20 @@ class TemporaryGitCheckout:
     def __exit__(self, *exc):
         if self.original_branch:
             self.original_branch.checkout()
+
+
+class OptionalLock:
+    def __init__(self, lock, should_lock=True):
+        self.lock = lock
+        self.should_lock = should_lock
+
+    def __enter__(self):
+        if self.should_lock:
+            self.lock.acquire()
+
+    def __exit__(self, *exc):
+        if self.should_lock:
+            self.lock.release()
 
 
 class DatasetStageItem:
@@ -71,60 +84,64 @@ class Repository:
         self.dvc_repo = dvc.repo.Repo(self.repo_dir)
         self.dataset_stage = []
         self.commit = commit
+        self.lock = fasteners.InterProcessLock(self.repo_dir / '.dvc-pandas.lock')
 
-    def load_dataset(self, identifier: str, skip_pull_if_exists=False) -> Dataset:
+    def load_dataset(self, identifier: str, skip_pull_if_exists=False, lock=True) -> Dataset:
         """
         Load dataset with the given identifier from the given repository.
 
         If `skip_pull_if_exists` is True, does not update the dataset if a parquet file exists for the identifier,
         regardless of the content.
         """
-        with TemporaryGitCheckout(self.git_repo, self.commit):
-            parquet_path = self.repo_dir / (identifier + '.parquet')
-            if not (parquet_path.exists() and skip_pull_if_exists):
-                logger.debug(f"Pull dataset {parquet_path} from DVC")
-                self.dvc_repo.pull(str(parquet_path))
-            df = pd.read_parquet(parquet_path)
+        with OptionalLock(self.lock, lock):
+            with TemporaryGitCheckout(self.git_repo, self.commit):
+                parquet_path = self.repo_dir / (identifier + '.parquet')
+                if not (parquet_path.exists() and skip_pull_if_exists):
+                    logger.debug(f"Pull dataset {parquet_path} from DVC")
+                    self.dvc_repo.pull(str(parquet_path))
+                df = pd.read_parquet(parquet_path)
 
-            # Get metadata (including units) from .dvc file
-            dvc_file_path = parquet_path.parent / (parquet_path.name + '.dvc')
-            yaml = YAML()
-            with open(dvc_file_path, 'rt') as file:
-                metadata = yaml.load(file).get('meta')
+                # Get metadata (including units) from .dvc file
+                dvc_file_path = parquet_path.parent / (parquet_path.name + '.dvc')
+                yaml = YAML()
+                with open(dvc_file_path, 'rt') as file:
+                    metadata = yaml.load(file).get('meta')
 
-            if metadata is None:
-                units = None
-            else:
-                units = metadata.pop('units', None)
+                if metadata is None:
+                    units = None
+                else:
+                    units = metadata.pop('units', None)
 
-            mtime = dvc_file_path.stat().st_mtime
-            modified_at = datetime.fromtimestamp(mtime, tz=timezone.utc)
+                mtime = dvc_file_path.stat().st_mtime
+                modified_at = datetime.fromtimestamp(mtime, tz=timezone.utc)
 
-            return Dataset(df, identifier, modified_at=modified_at, units=units, metadata=metadata)
+                return Dataset(df, identifier, modified_at=modified_at, units=units, metadata=metadata)
 
-    def load_dataframe(self, identifier: str, skip_pull_if_exists=False) -> pd.DataFrame:
+    def load_dataframe(self, identifier: str, skip_pull_if_exists=False, lock=True) -> pd.DataFrame:
         """
         Same as load_dataset, but only provides the DataFrame for convenience.
         """
-        dataset = self.load_dataset(identifier, skip_pull_if_exists)
+        dataset = self.load_dataset(identifier, skip_pull_if_exists, lock)
         return dataset.df
 
-    def has_dataset(self, identifier: str) -> bool:
+    def has_dataset(self, identifier: str, lock=True) -> bool:
         """
         Check if a dataset with the given identifier exists.
         """
-        with TemporaryGitCheckout(self.git_repo, self.commit):
-            dvc_file_path = self.repo_dir / (identifier + '.parquet.dvc')
-            return os.path.exists(dvc_file_path)
+        with OptionalLock(self.lock, lock):
+            with TemporaryGitCheckout(self.git_repo, self.commit):
+                dvc_file_path = self.repo_dir / (identifier + '.parquet.dvc')
+                return os.path.exists(dvc_file_path)
 
-    def pull_datasets(self):
+    def pull_datasets(self, lock=True):
         """
         Make sure the git repository is pulled to the latest version.
         """
-        logger.debug("Pull from git remote")
-        self.git_repo.remote().pull()
+        with OptionalLock(self.lock, lock):
+            logger.debug("Pull from git remote")
+            self.git_repo.remote().pull()
 
-    def push_dataset(self, dataset: Dataset):
+    def push_dataset(self, dataset: Dataset, lock=True):
         """
         Add the given dataset as a parquet file to DVC, create a commit and push it.
 
@@ -134,96 +151,100 @@ class Repository:
         This is a convenience method wrapping add() and push(). If the dataset stage (the one populated by add()) is not
         empty, this raises a ValueError.
         """
-        if self.dataset_stage:
-            raise ValueError("push_dataset was called with nonempty stage.")
-        if self.read_only:
-            raise ValueError("push_dataset was called while repository is read-only.")
+        with OptionalLock(self.lock, lock):
+            if self.dataset_stage:
+                raise ValueError("push_dataset was called with nonempty stage.")
+            if self.read_only:
+                raise ValueError("push_dataset was called while repository is read-only.")
 
-        self.pull_datasets()
-        parquet_path = self.repo_dir / (dataset.identifier + '.parquet')
-        self.add(dataset)
-        try:
-            self.push()
-        except Exception:
-            # Restore original data
-            # git reset --hard origin/master
-            logger.debug("Hard-reset master branch to origin/master")
-            self.git_repo.head.reset('origin/master', index=True, working_tree=True)
+            self.pull_datasets(lock=False)
+            parquet_path = self.repo_dir / (dataset.identifier + '.parquet')
+            self.add(dataset, lock=False)
+            try:
+                self.push(lock=False)
+            except Exception:
+                # Restore original data
+                # git reset --hard origin/master
+                logger.debug("Hard-reset master branch to origin/master")
+                self.git_repo.head.reset('origin/master', index=True, working_tree=True)
 
-            logger.debug(f"Checkout {parquet_path} from DVC")
-            self.dvc_repo.checkout(str(parquet_path), force=True)
+                logger.debug(f"Checkout {parquet_path} from DVC")
+                self.dvc_repo.checkout(str(parquet_path), force=True)
 
-            # Remove rolled back stuff that may be left on the remote
-            # self.dvc_repo.gc(all_commits=True, cloud=True, remote=self.dvc_remote)
-            # TODO: Before reinstating the previous line, make sure gc doesn't delete blobs for which we don't have a
-            # git commit because our git repository is outdated.
-            raise
-        finally:
-            # Remove old version (or new if we rolled back) from cache
-            logger.debug("Collect garbage in local DVC repository")
-            self.dvc_repo.gc(workspace=True)
+                # Remove rolled back stuff that may be left on the remote
+                # self.dvc_repo.gc(all_commits=True, cloud=True, remote=self.dvc_remote)
+                # TODO: Before reinstating the previous line, make sure gc doesn't delete blobs for which we don't have
+                # a git commit because our git repository is outdated.
+                raise
+            finally:
+                # Remove old version (or new if we rolled back) from cache
+                logger.debug("Collect garbage in local DVC repository")
+                self.dvc_repo.gc(workspace=True)
 
-    def add(self, dataset: Dataset):
+    def add(self, dataset: Dataset, lock=True):
         """Create or update parquet file from dataset, but do not create .dvc file, commit or push."""
-        if self.read_only:
-            raise ValueError("add was called while repository is read-only.")
+        with OptionalLock(self.lock, lock):
+            if self.read_only:
+                raise ValueError("add was called while repository is read-only.")
 
-        parquet_path = self.repo_dir / (dataset.identifier + '.parquet')
-        os.makedirs(parquet_path.parent, exist_ok=True)
-        dataset.df.to_parquet(str(parquet_path))
-        self.dataset_stage.append(DatasetStageItem(dataset))
+            parquet_path = self.repo_dir / (dataset.identifier + '.parquet')
+            os.makedirs(parquet_path.parent, exist_ok=True)
+            dataset.df.to_parquet(str(parquet_path))
+            self.dataset_stage.append(DatasetStageItem(dataset))
 
-    def push(self):
+    def push(self, lock=True):
         """Upload, commit and push the staged files and clear the stage."""
-        if self.read_only:
-            raise ValueError("add was called while repository is read-only.")
+        with OptionalLock(self.lock, lock):
+            if self.read_only:
+                raise ValueError("add was called while repository is read-only.")
 
-        for stage_item in self.dataset_stage:
-            path = self.repo_dir / (stage_item.identifier + '.parquet')
+            for stage_item in self.dataset_stage:
+                path = self.repo_dir / (stage_item.identifier + '.parquet')
 
-            # Remove old .dvc file (if it exists) and replace it with a new one
-            dvc_file_path = path.parent / (path.name + '.dvc')
-            if dvc_file_path.exists():
-                logger.debug(f"Remove file {dvc_file_path} from DVC")
-                self.dvc_repo.remove(str(dvc_file_path))
+                # Remove old .dvc file (if it exists) and replace it with a new one
+                dvc_file_path = path.parent / (path.name + '.dvc')
+                if dvc_file_path.exists():
+                    logger.debug(f"Remove file {dvc_file_path} from DVC")
+                    self.dvc_repo.remove(str(dvc_file_path))
 
-            logger.debug(f"Add file {path} to DVC")
-            self.dvc_repo.add(str(path))
+                logger.debug(f"Add file {path} to DVC")
+                self.dvc_repo.add(str(path))
 
-            logger.debug(f"Set metadata to {stage_item.metadata}")
-            set_dvc_file_metadata(dvc_file_path, stage_item.metadata)
+                logger.debug(f"Set metadata to {stage_item.metadata}")
+                set_dvc_file_metadata(dvc_file_path, stage_item.metadata)
 
-            # Add .dvc file and .gitignore to index
-            gitignore_path = dvc_file_path.parent / '.gitignore'
-            self.git_repo.index.add([str(dvc_file_path), str(gitignore_path)])
+                # Add .dvc file and .gitignore to index
+                gitignore_path = dvc_file_path.parent / '.gitignore'
+                self.git_repo.index.add([str(dvc_file_path), str(gitignore_path)])
 
-            logger.debug(f"Push file {dvc_file_path} to DVC remote {self.dvc_remote}")
-            self.dvc_repo.push(str(dvc_file_path), remote=self.dvc_remote)
+                logger.debug(f"Push file {dvc_file_path} to DVC remote {self.dvc_remote}")
+                self.dvc_repo.push(str(dvc_file_path), remote=self.dvc_remote)
 
-        # Commit and push
-        if self._need_commit():
-            identifiers = [stage_item.identifier for stage_item in self.dataset_stage]
-            commit_message = f"Update {', '.join(identifiers)}"
-            logger.debug(f"Create commit: {commit_message}")
-            self.git_repo.index.commit(commit_message)
-            logger.debug("Push to git repository")
-            git_push(self.git_repo)
-        else:
-            logger.debug("No commit needed")
+            # Commit and push
+            if self._need_commit(lock=False):
+                identifiers = [stage_item.identifier for stage_item in self.dataset_stage]
+                commit_message = f"Update {', '.join(identifiers)}"
+                logger.debug(f"Create commit: {commit_message}")
+                self.git_repo.index.commit(commit_message)
+                logger.debug("Push to git repository")
+                git_push(self.git_repo)
+            else:
+                logger.debug("No commit needed")
 
-        self.dataset_stage = []
+            self.dataset_stage = []
 
     @property
     def read_only(self):
         return self.commit is not None
 
-    def _need_commit(self) -> bool:
-        if not self.git_repo.is_dirty():
-            return False
-        # We don't commit if the only files changed are .gitignore files. DVC sometimes reorders the lines in .gitignore
-        # even though no file has changed.
-        diffs = self.git_repo.index.diff(self.git_repo.head.commit)
-        return any(os.path.basename(diff.a_path) != '.gitignore' for diff in diffs)
+    def _need_commit(self, lock=True) -> bool:
+        with OptionalLock(self.lock, lock):
+            if not self.git_repo.is_dirty():
+                return False
+            # We don't commit if the only files changed are .gitignore files. DVC sometimes reorders the lines in
+            # .gitignore even though no file has changed.
+            diffs = self.git_repo.index.diff(self.git_repo.head.commit)
+            return any(os.path.basename(diff.a_path) != '.gitignore' for diff in diffs)
 
 
 # FIXME: Make common base class instead of extending Repository
